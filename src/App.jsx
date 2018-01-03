2f118bb02904beee6aaf7d633398d9d691fc41d8
dhanyn10 <dhanyn.dhan@gmail.com>
2018-01-03 19:31:29 +0700

    upload

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Link,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+
+//style
+import '../node_modules/minini/css/style/global.css';
+import '../node_modules/minini/css/style/container.css';
+import '../node_modules/minini/css/component/navbar.css';
+import '../node_modules/minini/css/style/responsive.css';
+
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+          <div>
+            <nav className="nav nav-g-md bg-green nav-md">
+              <div className="brand">
+                <div className="nav-brand-text text-white">Minini</div>
+              </div>
+              <ul className="navbar navnav right text-white">
+                <li className="nav-item">
+                  <Link className="nav-link" to="https://github.com/dhanyn10/minini">Github</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Home extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <center>
+                    <div className="jumper-2"></div>
+                    <h1>Coming Soon!</h1>
+                </center>
+            </div>
+        );
+    }
+}
